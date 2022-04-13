58eba2475e24bd11dcc2afa61b7ca9ec5cda7286
alejandroCinelli <alejandro.cinelli@hotmail.com>
2022-04-12 22:27:57 -0300

    clase02ed01

---
+++ src/components/DaisyFooter.jsx
@@ -0,0 +1,27 @@
+const DaisyFooter = () => {
+  return (
+    <footer className="footer p-10 bg-neutral text-neutral-content">
+    <div>
+      <span className="footer-title">Services</span> 
+      <a href="f" className="link link-hover">Branding</a>
+      <a href="e" className="link link-hover">Design</a>
+      <a href="as" className="link link-hover">Marketing</a>
+      <a href="fas" className="link link-hover">Advertisement</a>
+    </div> 
+    <div>
+      <span className="footer-title">Company</span> 
+      <a href="sf" className="link link-hover">About us</a>
+      <a href="asf" className="link link-hover">Contact</a>
+      <a href="saf" className="link link-hover">Jobs</a>
+      <a href="saf" className="link link-hover">Press kit</a>
+    </div> 
+    <div>
+      <span className="footer-title">Legal</span> 
+      <a href="saf" className="link link-hover">Terms of use</a>
+      <a href="asf" className="link link-hover">Privacy policy</a>
+      <a href="saf" className="link link-hover">Cookie policy</a>
+    </div>
+  </footer>
+  )
+}
+export default DaisyFooter
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,6 @@
+const NavBar = () => {
+  return (
+    <div className="text-3xl font-bold underline"> NavBar </div>
+  )
+}
+export default NavBar
